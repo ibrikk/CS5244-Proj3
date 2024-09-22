f84450daac3445c87f567153504cd043ef553d36
Ibrahim Khalilov <ibrahim.khalilov@yahoo.com>
2024-09-21 20:01:38 -0400

    fix color bg issue

--- client/src/components/HomePage.tsx
+++ client/src/components/HomePage.tsx
@@ -81,6 +81,7 @@ const HomePage = () => {
       <section className="homepage-card-section grid grid-full">
         <h3 className="homepage-category-name">New Releases</h3>
         <div className="cards-container grid grid-full">
+          {/* TODO: Home Category List insert here */}
           {/* Example Card */}
           <div className="card">
             <div className="book-image-read-btn-container">
